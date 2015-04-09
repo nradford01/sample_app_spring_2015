8d1680b4e93f894bff364ded276e509d0be87ae6
Carlos Garcia <carlos.garcia@carlosagp.com>
2015-04-09 12:17:20 -0600

    DRYed the Ipinfo class

--- lib/apis/ipinfo.rb
+++ lib/apis/ipinfo.rb
@@ -4,15 +4,16 @@ class Ipinfo
 
   def get_info(format=nil)
     @last_request ||= self.class.get("/json")
+    format.nil? ? @last_request : JSON.parse(@last_request.body)
   end
 
   def get_coordinates()
-    response = JSON.parse(self.get_info().body)
+    response = self.get_info(:json)
     response['loc'].split(',')
   end
 
   def get_city()
-    response = JSON.parse(self.get_info().body)
+    response = self.get_info(:json)
     response['city']
   end
 
