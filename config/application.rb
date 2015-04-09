d558480bfa152e5ca1d2383ef045f9782962a40f
Carlos Garcia <carlos.garcia@carlosagp.com>
2015-04-09 11:55:26 -0600

    Added commentary to lib configuration

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module SampleApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # config.autoload_paths << Rails.root.join('lib', 'apis', '**')
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
